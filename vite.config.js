7295a970372c5fd82f8d65ab2c4638044447fb93
TmRAaEx <tmr.aaex@gmail.com>
2024-12-11 16:00:08 +0100

    Deployment test 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://tmraaex.github.io/e-commerce-reactVersion//e-commerce-reactVersion/"
+  base: "https://tmraaex.github.io/e-commerce-reactVersion/"
 })
